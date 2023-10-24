032f51e66cd954219de08909c8a2e6f9f755d5e6
Mohamed Amine Rguig <amine.mohamed.rg@gmail.com>
2023-10-24 16:13:01 +0100

    client api dev + test

--- src/app/api/client/[id]/route.ts
+++ src/app/api/client/[id]/route.ts
@@ -1,17 +1,47 @@
 import { NextRequest, NextResponse } from "next/server"
 import { Client } from "../../../../../models/Client";
 import { connectToDatabase } from "../../../../../db/connection";
+import { Sale, SaleModel } from "../../../../../models/Sale";
+import { getPaginatedResult } from "@/utils/util";
 
 
 connectToDatabase()
 // GET CLIENT BY ID
 export const GET = async (req: NextRequest, { params }: { params: { id: string } }) => {
+
+    const url = new URL(req.url);
+    const searchParams = new URLSearchParams(url.search);
+    const page = Number(searchParams.get("page")) || 1;
+    const limit = Number(searchParams.get("limit")) || 10;
     try {
-        const client = await Client.findById(params.id).exec();
+        const clientId = params.id
+        const client = await Client.findById(clientId).exec();
         if (!client) {
             return NextResponse.json({ message: "Client not found" }, { status: 404 });
         }
-        return NextResponse.json(client, { status: 200 });
+        // count the sales made by the client
+        const clientSalesCount = await Sale.find({ "client": clientId }).populate({ path: "items.article", select: 'img weight' })
+        const { startIndex, results } = getPaginatedResult(page, limit, clientSalesCount.length)
+        let totalPayment = 0;
+        let totalPaid = 0;
+        let clientSales;
+        if (clientSalesCount.length > 0) {
+            clientSales = await Sale.find({ "client": clientId }).populate({ path: "items.article", select: 'img weight' }).skip(startIndex).limit(limit)
+
+            // calculate the total payment that client made
+            totalPayment = clientSalesCount?.reduce((total, item) => {
+                return total + item.totalPrice
+            }, 0);
+
+            // calculate the total amount that the client paid
+            totalPaid = clientSalesCount?.reduce((total, item) => {
+                return total + item.paid
+            }, 0);
+        }
+        const nonPaid = (totalPayment - totalPaid);
+        results.result = clientSales ?? []
+        results.total = clientSalesCount.length
+        return NextResponse.json({ client, ...results, totalPayment, nonPaid }, { status: 200 });
     } catch (error) {
         return NextResponse.json(
             { message: "Something went wrong" },
