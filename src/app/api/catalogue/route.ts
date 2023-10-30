f9d65bae02f6465950547f6f75ee37b134642c4b
Mohamed Amine Rguig <amine.mohamed.rg@gmail.com>
2023-10-30 15:08:58 +0100

    user can get a list of catalogue with pagination

--- src/app/api/catalogue/route.ts
+++ src/app/api/catalogue/route.ts
@@ -1,8 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Catalogue } from "../../../../models/Catalogue";
 import { connectToDatabase } from "../../../../db/connection";
+import { getPaginatedResult } from "@/utils/util";
 
 connectToDatabase()
+// get all catalog with pagination
+export const GET = async (req: NextRequest) => {
+
+    // getting the page number and limit from the url
+    const url = new URL(req.url);
+    const searchParams = new URLSearchParams(url.search);
+    const page = Number(searchParams.get("page")) || 1;
+    const limit = Number(searchParams.get("limit")) || 10;
+    const totalCatalog = await Catalogue.estimatedDocumentCount()
+
+    // calling a method that return start index and end index, 
+    // and results object that may contain next and previous page
+    const { startIndex, results } = getPaginatedResult(page, limit, totalCatalog)
+
+    try {
+        const catalogue = await Catalogue.find().skip(startIndex).limit(limit)
+        results.total = totalCatalog;
+        results.result = catalogue;
+        return NextResponse.json(results, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ message: "Something went wrong" }, { status: 500 })
+    }
+}
+
+// create a catalog
 export const POST = async (req: NextRequest, res: NextResponse) => {
 
     try {
